69e660724d22f9a0b301e29d0badf1f815b119e9
Miko Väli <mikovali@gmail.com>
2020-09-06 14:52:06 +0300

    Prev/next buttons to images

--- app/src/main/java/com/sensorfields/livingscreen/android/album/list/MediaItemImageViewFragment.kt
+++ app/src/main/java/com/sensorfields/livingscreen/android/album/list/MediaItemImageViewFragment.kt
@@ -3,13 +3,16 @@ package com.sensorfields.livingscreen.android.album.list
 import android.graphics.Point
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.navGraphViewModels
 import com.bumptech.glide.Glide
 import com.sensorfields.livingscreen.android.R
 import com.sensorfields.livingscreen.android.databinding.MediaItemImageViewFragmentBinding
+import com.sensorfields.livingscreen.android.domain.MediaItem
 import com.sensorfields.livingscreen.android.producer
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -36,9 +39,18 @@ class MediaItemImageViewFragment : Fragment(R.layout.media_item_image_view_fragm
         )
     }
 
+    private var state: MediaItemViewState? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupViews()
         setupViewModel()
+    }
+
+    private fun setupViews() {
+        viewBinding.previousButton.setOnClickListener { onPreviousButtonClicked() }
+        viewBinding.nextButton.setOnClickListener { onNextButtonClicked() }
+        viewBinding.detailsButton.setOnClickListener { onDetailsButtonClicked() }
     }
 
     private fun setupViewModel() {
@@ -46,9 +58,38 @@ class MediaItemImageViewFragment : Fragment(R.layout.media_item_image_view_fragm
     }
 
     private fun onState(state: MediaItemViewState) {
+        this.state = state
+        viewBinding.previousButton.isInvisible = state.previous == null
+        viewBinding.nextButton.isInvisible = state.next == null
         viewBinding.imageView.contentDescription = state.current.fileName
         Glide.with(viewBinding.imageView)
             .load("${state.current.baseUrl}=w${size.x}-h${size.y}")
             .into(viewBinding.imageView)
+    }
+
+    private fun onPreviousButtonClicked() {
+        state?.previous?.let { item -> navigateToMediaItemView(item) }
+    }
+
+    private fun onNextButtonClicked() {
+        state?.next?.let { item -> navigateToMediaItemView(item) }
+    }
+
+    private fun navigateToMediaItemView(item: MediaItemGridState.Item) {
+        val directions = when (item.type) {
+            is MediaItem.Type.Photo -> {
+                MediaItemImageViewFragmentDirections.mediaItemImageView(item.index)
+            }
+            is MediaItem.Type.Video -> {
+                MediaItemViewFragmentDirections.mediaItemDetails(item.index)
+            }
+        }
+        findNavController().navigate(directions)
+    }
+
+    private fun onDetailsButtonClicked() {
+        findNavController().navigate(
+            MediaItemImageViewFragmentDirections.mediaItemDetails(args.index)
+        )
     }
 }
