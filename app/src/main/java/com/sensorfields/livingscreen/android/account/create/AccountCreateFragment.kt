dff985d332ca4de03b6a2f308ebd32450f6396bc
Miko Väli <mikovali@gmail.com>
2020-09-24 22:47:27 +0300

    Login UI

--- app/src/main/java/com/sensorfields/livingscreen/android/account/create/AccountCreateFragment.kt
+++ app/src/main/java/com/sensorfields/livingscreen/android/account/create/AccountCreateFragment.kt
@@ -1,42 +1,56 @@
 package com.sensorfields.livingscreen.android.account.create
 
 import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.viewModels
+import androidx.leanback.app.GuidedStepSupportFragment
+import androidx.leanback.widget.GuidanceStylist
+import androidx.leanback.widget.GuidedAction
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.sensorfields.livingscreen.android.R
 import com.sensorfields.livingscreen.android.SignInWithGoogle
-import com.sensorfields.livingscreen.android.databinding.AccountCreateFragmentBinding
 import com.sensorfields.livingscreen.android.producer
-import com.sensorfields.livingscreen.android.viewState
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import javax.inject.Provider
 
 @AndroidEntryPoint
-class AccountCreateFragment : Fragment(R.layout.account_create_fragment) {
+class AccountCreateFragment : GuidedStepSupportFragment() {
 
     @Inject
     lateinit var factory: Provider<AccountCreateViewModel>
 
     private val viewModel by viewModels<AccountCreateViewModel> { producer { factory.get() } }
 
-    private val viewBinding by viewState({ AccountCreateFragmentBinding.bind(it) })
-
     private val signInWithGoogle = registerForActivityResult(
         SignInWithGoogle(),
         ::onGoogleSignInCompleted
     )
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupViews()
+    override fun onCreateGuidance(savedInstanceState: Bundle?): GuidanceStylist.Guidance {
+        return GuidanceStylist.Guidance(
+            getString(R.string.application_name),
+            getString(R.string.account_create_description),
+            null,
+            ResourcesCompat.getDrawable(resources, R.mipmap.ic_launcher, null)
+        )
     }
 
-    private fun setupViews() {
-        viewBinding.googleSignInButton.setOnClickListener { onGoogleSignInButtonClicked() }
+    override fun onCreateActions(actions: MutableList<GuidedAction>, savedInstanceState: Bundle?) {
+        actions.add(
+            GuidedAction.Builder(requireContext())
+                .id(GOOGLE_SIGN_IN)
+                .title(R.string.common_signin_button_text_long)
+                .icon(R.drawable.common_google_signin_btn_icon_dark)
+                .build()
+        )
+    }
+
+    override fun onGuidedActionClicked(action: GuidedAction) {
+        when (action.id) {
+            GOOGLE_SIGN_IN -> onGoogleSignInButtonClicked()
+        }
     }
 
     private fun onGoogleSignInButtonClicked() {
@@ -51,3 +65,5 @@ class AccountCreateFragment : Fragment(R.layout.account_create_fragment) {
         findNavController().navigate(AccountCreateFragmentDirections.main())
     }
 }
+
+private const val GOOGLE_SIGN_IN = 1L
