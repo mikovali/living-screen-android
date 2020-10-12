a6c9ba51b172c107f97e064f5d8b9bf155259313
Miko Väli <mikovali@gmail.com>
2020-10-12 22:59:21 +0300

    Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,17 +66,18 @@ detekt {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0")
 
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
     implementation("androidx.fragment:fragment-ktx:1.3.0-beta01")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
     implementation("androidx.leanback:leanback:1.0.0")
     implementation("androidx.room:room-ktx:2.2.5")
     kapt("androidx.room:room-compiler:2.2.5")
 
     implementation("com.google.firebase:firebase-crashlytics-ktx:17.2.2")
-    implementation("com.google.firebase:firebase-analytics-ktx:17.5.0")
+    implementation("com.google.firebase:firebase-analytics-ktx:17.6.0")
 
     implementation("com.google.android.gms:play-services-auth:18.1.0")
 
@@ -87,7 +88,7 @@ dependencies {
 
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.7.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
 
     implementation("com.github.bumptech.glide:glide:4.11.0")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,8 @@ buildscript {
         google()
         jcenter()
     }
-
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("com.android.tools.build:gradle:4.1.0")
         classpath(kotlin("gradle-plugin", version = "1.4.10"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.29.1-alpha")
