0a46e48d50b7d3b48cf1c2939eee32d042fe5f20
Miko Väli <mikovali@gmail.com>
2020-11-30 17:33:43 +0200

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
     kotlin("android")
     kotlin("android.extensions")
     kotlin("kapt")
-    kotlin("plugin.serialization") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.20"
     id("androidx.navigation.safeargs.kotlin")
     id("dagger.hilt.android.plugin")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
     id("com.google.firebase.appdistribution")
-    id("io.gitlab.arturbosch.detekt") version "1.14.1"
+    id("io.gitlab.arturbosch.detekt") version "1.14.2"
 }
 
 android {
@@ -63,24 +63,24 @@ detekt {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
 
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
     implementation("androidx.fragment:fragment-ktx:1.3.0-beta01")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
-    implementation("androidx.leanback:leanback:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.leanback:leanback:1.1.0-alpha05")
     implementation("androidx.leanback:leanback-paging:1.1.0-alpha05")
-    implementation("androidx.paging:paging-common-ktx:3.0.0-alpha07")
-    implementation("androidx.paging:paging-runtime-ktx:3.0.0-alpha07")
-    implementation("androidx.room:room-ktx:2.2.5")
-    kapt("androidx.room:room-compiler:2.2.5")
+    implementation("androidx.paging:paging-common-ktx:3.0.0-alpha09")
+    implementation("androidx.paging:paging-runtime-ktx:3.0.0-alpha09")
+    implementation("androidx.room:room-ktx:2.3.0-alpha03")
+    kapt("androidx.room:room-compiler:2.3.0-alpha03")
 
-    implementation("com.google.firebase:firebase-crashlytics-ktx:17.2.2")
-    implementation("com.google.firebase:firebase-analytics-ktx:17.6.0")
+    implementation("com.google.firebase:firebase-crashlytics-ktx:17.3.0")
+    implementation("com.google.firebase:firebase-analytics-ktx:18.0.0")
 
-    implementation("com.google.android.gms:play-services-auth:18.1.0")
+    implementation("com.google.android.gms:play-services-auth:19.0.0")
 
     implementation("com.google.dagger:hilt-android:2.29.1-alpha")
     kapt("com.google.dagger:hilt-android-compiler:2.29.1-alpha")
@@ -93,7 +93,7 @@ dependencies {
 
     implementation("com.github.bumptech.glide:glide:4.11.0")
 
-    implementation("com.google.android.exoplayer:exoplayer-core:2.12.0")
-    implementation("com.google.android.exoplayer:extension-okhttp:2.12.0")
-    implementation("com.google.android.exoplayer:extension-leanback:2.12.0")
+    implementation("com.google.android.exoplayer:exoplayer-core:2.12.1")
+    implementation("com.google.android.exoplayer:extension-okhttp:2.12.1")
+    implementation("com.google.android.exoplayer:extension-leanback:2.12.1")
 }
